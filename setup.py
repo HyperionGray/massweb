85bf92e4601213f0332ea7f38a33ec3705002004
root <root@ip-172-31-33-75.us-west-2.compute.internal>
2014-04-21 22:21:29 +0000

    Reorg project

--- setup.py
+++ setup.py
@@ -1,21 +1,20 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
 
-    name = 'massweb'
-    version = '0.0.1'
-    description = 'Fast Web Fuzzing and Scanning'
-    long_description = 'Hyperion Gray\'s fast scanning and fuzzing module. Used in PunkSPIDER 3.0.'
-    url = 'https://bitbucket.org/acaceres/massweb'
-    license = 'Apache 2.0'
-    author = 'Alejandro Caceres'
-    author_email = 'contact@hyperiongray.com'
-    packages = find_packages()
+    name = 'massweb',
+    version = '0.0.1',
+    description = 'Fast Web Fuzzing and Scanning',
+    long_description = 'Hyperion Gray\'s fast scanning and fuzzing module. Used in PunkSPIDER 3.0.',
+    url = 'https://bitbucket.org/acaceres/massweb',
+    license = 'Apache 2.0',
+    author = 'Alejandro Caceres',
+    author_email = 'contact@hyperiongray.com',
+    packages = find_packages(),
     include_package_data = True,
     classifiers = [ "Development Status :: 2 - Pre-Alpha",
                     'Intended Audience :: Developers',
                     'Programming Language :: Python :: 2.7',
                     'Programming Language :: Python']
-
 )
