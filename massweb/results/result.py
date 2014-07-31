2131cb4024a9f8196303a0d50f413001bf855264
punk <punk@punk-kali.punk-kali.hyperiongray.com>
2014-07-31 17:11:00 -0400

    fixed Result object to specify get or post and add data if post

--- massweb/results/result.py
+++ massweb/results/result.py
@@ -6,7 +6,12 @@ class Result(object):
     def __str__(self):
 
         url_parsed = urlparse(str(self.fuzzy_target))
-        to_ret = json.dumps({"url" : str(self.fuzzy_target), "results" : self.result_dic})
+        if self.fuzzy_target.ttype == "get":
+            to_ret = json.dumps({"url" : str(self.fuzzy_target), "results" : self.result_dic, "request_type" : "get"})
+
+        if self.fuzzy_target.ttype == "post":
+            to_ret = json.dumps({"url" : str(self.fuzzy_target), "data" : self.fuzzy_target.data, "results" : self.result_dic, "request_type" : "post"})
+
 
         return to_ret
 
