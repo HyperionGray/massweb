39b1a612d7d96d3e05edf15d3b0e457f4f4239f7
haxwithaxe (ckoepke) <me@haxwithaxe.net>
2015-01-05 15:23:14 -0500

    pep8 imports, Comments, FIXME's, and minor formatting changes all over ifuzzer.py. found big chunks of duplicate code

--- massweb/fuzzers/ifuzzer.py
+++ massweb/fuzzers/ifuzzer.py
@@ -1,9 +1,11 @@
-from massweb.payloads.payload import Payload
-from massweb.targets.target import Target
 from urlparse import parse_qs
 from urlparse import urlparse
 from urlparse import urlunparse
 from urllib import urlencode
+
+from massweb.payloads.payload import Payload
+from massweb.targets.target import Target
+
 
 class iFuzzer(object):
 
@@ -35,67 +37,71 @@ class iFuzzer(object):
         pass
 
     def replace_param_value(self, url, param, replacement_string):
-        '''Replace a parameter in a url with another string. Returns
-        a fully reassembled url as a string.'''
-
+        """Replace a parameter in a url with another string. Returns
+        a fully reassembled url as a string.
+	FIXME: Fill in this doc comment
+	url ___
+	param ___
+	replacement_string ___
+	"""
         url_parsed = urlparse(url)
         query_dic = parse_qs(url_parsed.query)
-        #!potential bug here? This expects a list, but seems to work        
+        #FIXME: Clarify with alex: !potential bug here? This expects a list, but seems to work        
         query_dic[param] = [replacement_string for x in query_dic[param]]
 
-        #this incidentally will also automatically url-encode the payload (thanks urlencode!)
-        #!might cause some incorrect query params and keys with utf-8, needs more testing
+	# FIXME: Duplicate code
+        # This incidentally will also automatically url-encode the payload (thanks urlencode!)
+        #FIXME: Clarify with alex: !might cause some incorrect query params and keys with utf-8, needs more testing
         str_query_dic = {}
         for k, v in query_dic.iteritems():
             str_query_dic[unicode(k).encode('utf-8', 'replace')] = [x.encode('utf-8', 'replace') for x in v]
-
         query_reassembled = urlencode(str_query_dic, doseq = True)
-
-        #3rd element is always the query, replace query with our own
+        # 3rd element is always the query, replace query with our own
         url_list_parsed = list(url_parsed)
         url_list_parsed[4] = query_reassembled
         url_parsed_q_replaced = tuple(url_list_parsed)
         url_reassembled = urlunparse(url_parsed_q_replaced)
-
         return url_reassembled
+	#END Duplicate code
     
     def append_to_param(self, url, param, append_string):
         '''Append a value to a parameter'''
 
         url_parsed = urlparse(url)
         query_dic = parse_qs(url_parsed.query)
-        #!potential bug here? This expects a list, but seems to work        
+        #FIXME: Clarify with alex: !potential bug here? This expects a list, but seems to work        
         query_dic[param] = [x + append_string for x in query_dic[param]]
 
+	#FIXME: Duplicate code
         #this incidentally will also automatically url-encode the payload (thanks urlencode!)
         #!might cause some incorrect query params and keys with utf-8, needs more testing
         str_query_dic = {}
         for k, v in query_dic.iteritems():
             str_query_dic[unicode(k).encode('utf-8', 'replace')] = [x.encode('utf-8', 'replace') for x in v]
-
         query_reassembled = urlencode(str_query_dic, doseq = True)
-
         #3rd element is always the query, replace query with our own
         url_list_parsed = list(url_parsed)
         url_list_parsed[4] = query_reassembled
         url_parsed_q_replaced = tuple(url_list_parsed)
         url_reassembled = urlunparse(url_parsed_q_replaced)
-
         return url_reassembled
+	#END Duplicate code
 
     def determine_posts_from_targets(self, dedupe = True):
-
+	""" FIXME: fill in this doc comment
+	dedupe bool ___. Default True
+	"""
         self.mreq.get_post_requests_from_targets(self.targets)
         identified_posts = self.mreq.identified_post_requests            
-
-        #dedupe posts if relevant
+        # Dedupe posts if relevant
         identified_posts = list(set(identified_posts))
-
         for ip in identified_posts:
             if ip not in self.targets:
                 self.targets.append(ip)
 
     def fuzz(self):
+	""" FIXME: Doc comment """
+	#FIXME: use the appropriate exception type?
         raise Exception("Not Implemented Error")
 
 if __name__ == "__main__":
@@ -103,6 +109,6 @@ if __name__ == "__main__":
     param = "q"
     append_string= " AND 1=1"
     ifuz = iFuzzer()
-    print ifuz.append_to_param(url, param, append_string)
+    result = ifuz.append_to_param(url, param, append_string)
+    print(result)
 
-    
