879bb022de2914c85383043a394f8393a8b392ab
haxwithaxe (ckoepke) <me@haxwithaxe.net>
2015-01-08 13:21:26 -0500

    added comments and fixed exception types

--- massweb/payloads/bsqli_payload_group.py
+++ massweb/payloads/bsqli_payload_group.py
@@ -1,26 +1,27 @@
+""" """
+
 from massweb.payloads.bsqli_payload import BSQLIPayload
 from massweb.payloads.payload_group import PayloadGroup
 
 class BSQLIPayloadGroup(PayloadGroup):
+    """ """
 
     def __init__(self, true_payload, false_payload):
+        """ """
 
         if isinstance(true_payload, BSQLIPayload) and isinstance(false_payload, BSQLIPayload):
             pass
-        
         else:
-            raise Exception("input payloads must be of type BSQLIPayload")
-
+            raise TypeError("input payloads must be of type BSQLIPayload")
         if true_payload.payload_attributes["truth"] == True and false_payload.payload_attributes["truth"] == False:
             pass
         else:
-            raise Exception("true_payload must have an truth attribute of True and false_payload must have a truth attribute of False")
-
+            raise ValueError("true_payload must have an truth attribute of True and false_payload must have a truth attribute of False")
         self.true_payload = true_payload
         self.false_payload = false_payload
 
-if __name__ == "__main__":
-
-    payload_true = BSQLIPayload("dddd", {"truth" : False})
+"""
+    payload_false = BSQLIPayload("dddd", {"truth" : False})
     payload_true = BSQLIPayload("ddd333d", {"truth" : True})
-    bspg = BSQLIPayloadGroup()
+    bspg = BSQLIPayloadGroup(payload_true, payload_false)
+""" 
--- massweb/payloads/payload_group.py
+++ massweb/payloads/payload_group.py
@@ -1,11 +1,18 @@
-from massweb.payloads.payload import Payload
+""" """
+
 
 class PayloadGroup(object):
+    """ PayloadGroup Class: groups of individual Payload objects"""
 
     def __init__(self, payloads, check_type_list = []):
-        
+        """ FIXME Fill out this docstring
+        payloads    ?list? of payload objects.
+        check_type_list list of types of checks to run. Default [].
+        """
         self.payloads = payloads
-    
+
     def add_payload(self, payload):
-        
+        """ Add a Payloadobject to this group.
+        payload Payload object
+        """
         self.payloads.append(payload)
