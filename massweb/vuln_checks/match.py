43afc1c0f2071e52384066a274dd761fc7c02279
haxwithaxe (ckoepke) <me@haxwithaxe.net>
2015-01-13 15:14:12 -0500

    comments and fixes for vuln_chacks. also added unittests for vuln_checks.

--- massweb/vuln_checks/match.py
+++ massweb/vuln_checks/match.py
@@ -1,41 +1,46 @@
+""" Helper functions for matching parts of strings """
+
 import sys
 import requests
 import traceback
-from bs4 import BeautifulSoup, SoupStrainer
+from bs4 import BeautifulSoup
 
-def match_string(_input, match):
-
-    if match in _input:
+def match_string(str_in, match):
+    """ Check if a string is found in another string.
+        str_in  Input string.
+        match   String to find in str_in.
+        returns True if a match is found and False if no match is found. """
+    if match in str_in:
         return True
-
     else:
         return False
 
-def match_strings(_input, match_list):
-
+def match_strings(str_in, match_list):
+    """ Check if any of a list of strings is found in another string
+        str_in      input string.
+        match_list  list of strings to find in str_in.
+        returns     True if a match is found and False if no match is found."""
     for match in match_list:
-        if match in _input:
+        if match in str_in:
             return True
-
     return False
 
-def parse_match(_input, tag, match):
-
-    for script in BeautifulSoup(_input, 'html5lib').findAll('script'):
-
+def parse_match(str_in, tag, match):
+    """ Parse HTML string for a particular tag and attempt to find a matching
+        string in the contents of that tag.
+        Currently is hardcoded for 'script' tags.
+        str_in  input string.
+        tag     html tag to parse for.
+        match   string to match against the contents of the tags.
+        returns True if a match is found and False if no match is found. """
+    #FIXME: tag is unused should it be used in place of 'script'?
+    for script in BeautifulSoup(str_in, 'html5lib').findAll('script'):
         try:
             script_text = script.get_text()
-            if match in script_text:
+            if match_string(script_text, match):
                 return True
-
-        except:
+        except: #FIXME: specify exception types
+            #FIXME: send to logger not stdout/stderr
             traceback.print_exc()
             continue
-
     return False
-
-if __name__ == "__main__":
-
-    x = requests.get("http://www.punkspider.org/").text    
-#    x = open("oaktree.file.out").read()
-    print parse_match(x, "script", "t")
--- massweb/vuln_checks/trav.py
+++ massweb/vuln_checks/trav.py
@@ -1,13 +1,21 @@
+""" Directory Traversal Checker """
+
 from massweb.vuln_checks.match import match_strings
 from massweb.vuln_checks.check import Check
 
+
 class TravCheck(Check):
+    """ Directory Traversal Checker: Checks for evidence of successful
+        directory traversal in result from fuzzers."""
+    #FIXME: Add brief description of the attack
 
     def __init__(self):
-
+        """ Initialize the object and normalize the strings used to check for
+            vulnerability in the response """
         self.vuln_string = ["root:x:", "[font]"]
 
     def check(self, content):
-
+        """ Check the string returned by the fuzzer (content) against the list
+            of strings indicating vulnerability. """
         content = content.lower()
         return match_strings(content, self.vuln_string)
