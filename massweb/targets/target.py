406b3fd82785e0de8ca4ba1ae4c090395963fde9
haxwithaxe (ckoepke) <me@haxwithaxe.net>
2015-01-21 15:13:06 -0500

    added path and domain properties to the Target object to help avoid reimplementation

--- massweb/targets/target.py
+++ massweb/targets/target.py
@@ -1,14 +1,18 @@
 """ Target type/prototype """
 
+from urlparse import urlparse
+
 class Target(object):
     """ Target prototype:
-        contains the url, request type, and ?data?"""
-        #FIXME: wtf is data ^
+        contains the url, request type, and data (POST request payload as dict)
+    """
 
     def __eq__(self, other):
         """ Test whether this object and another Target object are equal based
-            on the url, request type and ?data?
-            other   Target object to compare to this object."""
+            on the url, request type, and data contents.
+
+        other   Target object to compare to this object.
+        """
         if not isinstance(other, Target):
             raise TypeError("Must provide Target or subclass of Target for comparison.")
         return (self.url == other.url and
@@ -16,7 +20,7 @@ class Target(object):
                 self.data == other.data)
 
     def __hash__(self):
-        """ Returns a hash of the url, request type, and ?data? """
+        """ Returns a hash of the url, request type, and data """
         return hash((self.url, self.ttype, str(self.data)))
 
     def __unicode__(self):
@@ -28,9 +32,9 @@ class Target(object):
         return unicode(self).encode('utf-8', 'replace')
 
     def __init__(self, url, data=None, ttype="get"):
-        """ Initialize a Target?:
+        """ Initialize a Target object:
             url     unicode object containing the location of the target.
-            data    ?data?
+            data    POST request payload as a dict.
             ttype   HTTP request type (get,post). Default "get". """
         #FIXME: why so picky about unicode?
         if not isinstance(url, unicode):
@@ -38,6 +42,23 @@ class Target(object):
         self.url = url
         self.ttype = ttype
         self.data = data
+
+    @property
+    def domain(self):
+        """ The domain part of the URL of this Target. """
+        # get everything but the uri prefix and path
+        domain_raw = self._parse().netloc
+        # strip off the port if it has one
+        domain = domain_raw.split(":", 1)[0]
+        return domain
+
+    @property
+    def path(self):
+        """ return the path portion of the URL of this Target. """
+        return self._parse().path
+
+    def _parse(self):
+        return urlparse(unicode(self))
 
 
 
