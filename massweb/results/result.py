b5129ce99f684822c62c2e2d99c03c4a5a5c9fe9
haxwithaxe (ckoepke) <me@haxwithaxe.net>
2015-01-29 21:04:58 -0500

    minor style fixes

--- massweb/results/result.py
+++ massweb/results/result.py
@@ -2,27 +2,29 @@
 
 import json
 
+
 class Result(object):
     """ Result type: encapsulates information relevant to the results
         of a test. """
 
     def __unicode__(self):
-        """ Returns  this object in a json formatted string. """
+        """ Return this object in a json formatted string. """
         # Assuming: self.fuzzy_target.ttype == "get" or
         #  self.fuzzy_target.ttype == "post"
-        to_ret = json.dumps({"url" : unicode(self.fuzzy_target),
-                             "data" : self.fuzzy_target.data,
-                             "results" : self.result_dic,
-                             "request_type" : self.fuzzy_target.ttype,
-                             "fuzzy_param" : self.fuzzy_target.fuzzy_param})
+        to_ret = json.dumps({"url": unicode(self.fuzzy_target),
+                             "data": self.fuzzy_target.data,
+                             "results": self.result_dic,
+                             "request_type": self.fuzzy_target.ttype,
+                             "fuzzy_param": self.fuzzy_target.fuzzy_param})
         return to_ret
 
     def __str__(self):
-        """ Returns a utf-8 encoded string """
+        """ Return a utf-8 encoded string. """
         return unicode(self).encode('utf-8', 'replace')
 
     def __init__(self, fuzzy_target, result_dic=None):
         """ Initialize a Result object.
+
         fuzzy_target    FuzzyTarget object.
         result_dic      dict containing keys of types of chercks and bool
                             values indicating success or failure. Default None.
