aeae61ef2b86390af1c7ed4f0be7ba84c9dc315f
haxwithaxe (ckoepke) <me@haxwithaxe.net>
2015-02-18 13:23:52 -0500

    conf.py had some pebkac errors which made alabaster not work. should be fixed now

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -16,8 +16,6 @@ import sys
 import os
 import alabaster
 
-html_theme_path = [alabaster.get_path(), '../env/lib/python2.7/site-packages/alabaster']
-html_theme = 'alabaster'
 html_sidebars = {'**': ['about.html', 'navigation.html', 'searchbox.html', 'donate.html',]}
 
 # If extensions (or modules to document with autodoc) are in another directory,
@@ -108,7 +106,8 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+#html_theme = 'default'
+html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -117,6 +116,7 @@ html_theme = 'default'
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
+html_theme_path = [alabaster.get_path(), '../env/lib/python2.7/site-packages/alabaster']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
@@ -154,6 +154,7 @@ html_static_path = ['_static']
 
 # Custom sidebar templates, maps document names to template names.
 #html_sidebars = {}
+html_sidebars = {'**': ['about.html', 'navigation.html', 'searchbox.html', 'donate.html',]}
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
