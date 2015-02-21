d555909ff47daf19c2c427a6b358ea5c51df25f7
haxwithaxe (ckoepke) <me@haxwithaxe.net>
2015-02-20 20:03:04 -0500

    added scripts containing code in the wiki to the test directory. they are not unit tests.

---
+++ test/docs/wiki_usage_test_5.py
@@ -0,0 +1,4 @@
+from massweb.fuzzers.web_fuzzer import WebFuzzer
+wf = WebFuzzer()
+target_1 = Target(u"http://www.hyperiongray.com")
+target_2 = Target(u"http://course.hyperiongray.com/vuln1", data = {"password" : "blah"}, ttype = "post")
